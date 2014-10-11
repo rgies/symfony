6515d681b8d0404e09d0ed9dae96ef8aa392abea
rgies <mail@rgies.de>
2014-10-11 17:18:23 +0200

    Add set_file_rights.sh for osx and linux

---
+++ bin/set_file_rights.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf app/cache/*
+rm -rf app/logs/*
+
+HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+sudo chmod +a "$HTTPDUSER allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs src web vendor
+sudo chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
