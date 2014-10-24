6033729d30714808f67a16bf6befe34ed0aefedb
rgies <mail@rgies.de>
2014-10-24 13:08:43 +0200

    Improve set_file_rights.sh

--- bin/set_file_rights.sh
+++ bin/set_file_rights.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-rm -rf app/cache/*
-rm -rf app/logs/*
+ROOTPATH=`pwd`/`dirname $0`/..
+
+rm -rf $ROOTPATH/app/cache/*
+rm -rf $ROOTPATH/app/logs/*
 
 HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-sudo chmod +a "$HTTPDUSER allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs src web vendor
-sudo chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
+if [ "$HTTPDUSER" == "" ]; then
+    echo '!!! Webserver process not found, please check if your webserver is running and try again.'
+    exit
+fi
+
+echo "Set file permitions to $HTTPDUSER !"
+sudo chmod +a "$HTTPDUSER allow delete,write,append,file_inherit,directory_inherit" $ROOTPATH/app $ROOTPATH/src $ROOTPATH/web $ROOTPATH/vendor
+sudo chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" $ROOTPATH/app/cache $ROOTPATH/app/logs
