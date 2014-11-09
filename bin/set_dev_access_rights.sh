d467ee653b565b2bd106291d37d41192bc4944ab
rgies <mail@rgies.de>
2014-11-09 20:56:53 +0100

    Improve set_dev_access_rights.sh

--- bin/set_dev_access_rights.sh
+++ bin/set_dev_access_rights.sh
@@ -13,5 +13,6 @@ fi
 
 echo "Set file permitions to $HTTPDUSER and owner to `whoami` !"
 sudo chown -R "`whoami`" $ROOTPATH
+sudo chmod 0777 $ROOTPATH/app/console
 sudo chmod -R +a "$HTTPDUSER allow delete,write,append,file_inherit,directory_inherit" $ROOTPATH/app $ROOTPATH/src $ROOTPATH/web $ROOTPATH/vendor
 sudo chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" $ROOTPATH/app/cache $ROOTPATH/app/logs
